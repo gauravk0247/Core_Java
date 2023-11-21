bdf00e87d71d1d4b9766cad2c8a5b8dbb84f13f8
gauravk0247 <gauravkhairnar2106@gmail.com>
2023-11-21 14:44:00 +0530

    Wrapper Class

--- Inner Class/Demo8.java
+++ Inner Class/Demo8.java
@@ -1,6 +1,20 @@
 // 20/11/2023
 class Outer{
-	int a = 300;
-	float b = 200;
+	void m2(){
+		Inner o1 = new Inner();
+		System.out.println(o1.a+o1.b);
+		o1.m1();
+	}
+	private class Inner{
+		private int a = 45;
+		private float b = 55.5f;
+		void m1(){
+			System.out.println("Inside m1 method");
+		}
+	}
 }
-class Demo8{}
+class Demo8{
+	public static void main(String[] args){
+	new Outer().m2();	
+	}
+}
