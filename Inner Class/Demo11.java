111258b04b988b3763fb16482fa6afe3bbf24b2d
gauravk0247 <gauravkhairnar2106@gmail.com>
2023-11-21 19:19:52 +0530

    Inners Class

--- Inner Class/Demo11.java
+++ Inner Class/Demo11.java
@@ -11,7 +11,8 @@ class Outer{
 }
 class Demo11{
 	public static void main(String[] args){
-		Outer.Inner i1 = new Outer.Inner();
-		i1.m1();
+		//Outer.Inner i1 = new Outer.Inner();
+		//i1.m1();
+		new Outer.Inner().m1();
 	}
 }
