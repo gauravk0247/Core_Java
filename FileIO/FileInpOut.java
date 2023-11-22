110fef1ea6fd6e8f9fb12575219294d874893f65
gauravk0247 <gauravkhairnar2106@gmail.com>
2023-11-22 21:47:03 +0530

    FileIO

--- FileIO/FileInpOut.java
+++ FileIO/FileInpOut.java
@@ -2,13 +2,18 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.FileOutputStream;
 
 class FileInpOut{
 	public static void main(String[] args)throws FileNotFoundException, IOException{
 		FileInputStream fis = new FileInputStream("D:\\Input.txt");
+		FileOutputStream fos = new FileOutputStream("D:\\Output.txt");
 		int info;
 		while((info=fis.read())!=-1){
 			System.out.println(" "+(char)info);
+			fos.write(info);
 		}
+		fis.close();
+		fos.close();
 	}
 }
