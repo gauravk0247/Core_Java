0a810258f118434ebecb364dfe20b611b3562930
gauravk0247 <gauravkhairnar2106@gmail.com>
2023-11-23 08:37:13 +0530

    FileIO

---
+++ FileIO/BuffeInOut.java
@@ -0,0 +1,28 @@
+// 23/11/23
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.FileOutputStream;
+import java.io.BufferedOutputStream;
+
+class BuffeInOut{
+	public static void main(String[] args) throws FileNotFoundException, IOException{
+		// Read
+		FileInputStream fis = new FileInputStream("D:\\BufferInput.txt");
+		BufferedInputStream bis = new BufferedInputStream(fis);
+		// Write
+		FileOutputStream fos = new FileOutputStream("BufferOutput.txt");
+		BufferedOutputStream bos = new BufferedOutputStream(fos);
+		int info;
+		while((info=bis.read())!=-1){
+			System.out.println((char)info);
+			bos.write(info);
+		}
+		bos.flush();
+		fis.close();
+		bis.close();
+		fos.close();
+		bos.close();
+	}
+}
---
+++ FileIO/BufferInOut.java
@@ -0,0 +1,29 @@
+// 23/11/23
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.FileOutputStream;
+import java.io.BufferedOutputStream;
+
+
+class BufferInOut{
+	public static void main(String[] args) throws FileNotFoundException, IOException{
+		// Read
+		FileInputStream fis = new FileInputStream("D:\\BufferInput.txt");
+		BufferedInputStream bis = new BufferedInputStream(fis);
+		// Write
+		FileOutputStream fos = new FileOutputStream("BufferOutput.txt");
+		BufferedOutputStream bos = new BufferedOutputStream(fos);
+		int info;
+		while((info=bis.read())!=-1){
+			System.out.println((char)info);
+			bos.write(info);
+		}
+		bos.flush();
+		fis.cloase();
+		bis.close();
+		fos.close();
+		bos.cloase();
+	}
+}
