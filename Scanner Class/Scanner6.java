a9498fb71bb13256c25472752d6e20419c10b2a7
gauravk0247 <gauravkhairnar2106@gmail.com>
2023-11-27 19:34:11 +0530

    Scanner Class

--- Scanner Class/Scanner6.java
+++ Scanner Class/Scanner6.java
@@ -1,2 +1,13 @@
 // 27/11/27
-class Scanner6{}
+import java.util.Scanner;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+class Scanner6{
+	public static void main(String[] args) throws FileNotFoundException{
+		FileReader fr = new FileReader("FileReader.txt");
+		Scanner s6 = new Scanner(fr);
+		while(s6.hasNext()){
+			System.out.println(s6.next());
+		}
+	}
+}
