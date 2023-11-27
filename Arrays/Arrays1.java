6606385596e51e764b25b563aac07fade4f7b690
gauravk0247 <gauravkhairnar2106@gmail.com>
2023-11-27 21:15:15 +0530

    Arrays

--- Arrays/Arrays1.java
+++ Arrays/Arrays1.java
@@ -2,11 +2,15 @@
 class Arrays1{
 	public static void main(String[] args){
 		int[] a = new int[3];
-		a[0]=10;
-		a[1]=20;
-		a[2]=30;
+		a[0]=100;
+		a[1]=200;
+		a[2]=300;
 		for(int i=0;i<=2;i++){
 		System.out.println(a[i]);			
 		}
+		System.out.println("------------------");
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
 	}
 }
---
+++ Arrays/Arrays2.java
@@ -0,0 +1,18 @@
+// 27/11/23
+class Arrays2{
+	public static void main(String[] args){
+		int[] a = {10, 20, 30};
+		for(int i=0;i<=2;i++){
+			System.out.println(a[i]);
+		}
+	}
+}
+
+class Arrays{
+	public static void main(String[] args){
+		int[] a = {10, 20, 30, 40};
+		for(int i=0;i<a.length;i++){
+			System.out.println(a[i]);
+		}
+	}
+}
