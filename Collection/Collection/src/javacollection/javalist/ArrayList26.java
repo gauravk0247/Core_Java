69e42e194f061df35f61ff2152a92067d5327ff1
gauravk0247 <gauravkhairnar2106@gmail.com>
2023-12-03 21:45:54 +0530

    Collection

--- Collection/Collection/src/javacollection/javalist/ArrayList26.java
+++ Collection/Collection/src/javacollection/javalist/ArrayList26.java
@@ -1,11 +1,25 @@
 package javacollection.javalist;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.ListIterator;
 
 public class ArrayList26 {
 
 	public static void main(String[] args) {
 		ArrayList<Emp> a1 = new ArrayList<Emp>();
 		a1.add(new Emp(101, "Gaurav"));
+		a1.add(new Emp(202, "Success"));
+		a1.add(new Emp(303, "Training"));
+		System.out.println(a1); 
+		//Collections.sot(a1); // --> It is not applicable to custom object
+		//System.out.println(a1);
+		ListIterator<Emp> i = a1.listIterator();
+		while(i.hasNext()) {
+			Emp e = i.next();
+			System.out.println(e.empId+" - "+e.empName);
+		}
+		
 	}
 }
