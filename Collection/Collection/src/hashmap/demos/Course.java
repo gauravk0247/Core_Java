1fb69120a91b1e092a0d2641adf3b6b141ee201d
gauravk0247 <gauravkhairnar2106@gmail.com>
2023-12-12 19:04:21 +0530

    HashMap

---
+++ Collection/Collection/src/hashmap/demos/Course.java
@@ -0,0 +1,11 @@
+package hashmap.demos;
+
+public class Course {
+	int courseId;
+	String courseName;
+	public Course(int courseId, String courseName) {
+		super();
+		this.courseId = courseId;
+		this.courseName = courseName;
+	}
+}
---
+++ Collection/Collection/src/hashmap/demos/Stud.java
@@ -0,0 +1,11 @@
+package hashmap.demos;
+
+public class Stud {
+	int studId;
+	String studName;
+	public Stud(int studId, String studName) {
+		super();
+		this.studId = studId;
+		this.studName = studName;
+	}
+}
