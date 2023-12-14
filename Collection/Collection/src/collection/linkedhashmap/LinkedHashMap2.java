303dbf613a24963331aca33403ede867f847176c
gauravk0247 <gauravkhairnar2106@gmail.com>
2023-12-14 15:33:42 +0530

    LinkedHashMap

--- Collection/Collection/src/collection/linkedhashmap/LinkedHashMap2.java
+++ Collection/Collection/src/collection/linkedhashmap/LinkedHashMap2.java
@@ -1,6 +1,9 @@
 package collection.linkedhashmap;
 
+import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class LinkedHashMap2 {
 
@@ -15,5 +18,17 @@ public class LinkedHashMap2 {
 		hm1.put(20, "B");
 		hm1.put(30, "C");
 		System.out.println(hm1);
+		LinkedHashMap<Integer, String> hm2 = new LinkedHashMap<Integer, String>();
+		hm2.putAll(hm1);
+		hm2.put(1, "P");
+		hm2.put(2, "Q");
+		hm2.put(3, "R");
+		System.out.println(hm2);
+		Set s = hm2.entrySet();
+		Iterator i = s.iterator();
+		while(i.hasNext()) {
+			Map.Entry<Integer, String> mp1 = (Map.Entry<Integer, String>)i.next();
+			System.out.println(mp1.getKey()+" - "+mp1.getValue());
+		}
 	}
 }
