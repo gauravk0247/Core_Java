3b1c24bd663c6d8b8f143d3f6c438bb3b9078bcb
gauravk0247 <gauravkhairnar2106@gmail.com>
2023-12-14 15:37:48 +0530

    TreeMap

---
+++ Collection/Collection/src/collection/treemap/TreeMap1.java
@@ -0,0 +1,14 @@
+package collection.treemap;
+
+import java.util.TreeMap;
+
+public class TreeMap1 {
+
+	public static void main(String[] args) {
+		TreeMap ts = new TreeMap();
+		ts.put(11, "Gaurav");
+		ts.put(33, "Krushna");
+		ts.put(22, "Darshan");
+		System.out.println(ts);
+	}
+}
